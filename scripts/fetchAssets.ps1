16c2bba92100bd53846ed19b39e0b7c914f03243
RoastChestnut <30802664+GES233@users.noreply.github.com>
2025-03-26 11:56:36 +0800

    Add tbs support with assets

--- scripts/fetchAssets.ps1
+++ scripts/fetchAssets.ps1
@@ -1,1 +1,17 @@
 # Copy TBS's assets into this repo.
+
+# TODO: implement get tbs's path and verify hash
+function Get-TheBrokenScrpt {
+    param (
+        LocalPath
+    )
+    # check file exist
+
+    # Only thebrokenscript-1.9.3-forge-1.20.1.jar
+    $fileHash = ' 79D2885AB7F74698313B21A5161C74ABC55A02F3E1069080FB3A4DEF31C3A460'
+}
+
+# TODO: unzip to temp
+
+# TODO: copy item recursively
+# use a mapper
