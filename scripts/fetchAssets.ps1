b887e5fe6ef3610913f2d9fff10fde055734b8af
RoastChestnut <30802664+GES233@users.noreply.github.com>
2025-03-26 12:40:29 +0800

    do some stuff

--- scripts/fetchAssets.ps1
+++ scripts/fetchAssets.ps1
@@ -1,7 +1,7 @@
 # Copy TBS's assets into this repo.
 
 # TODO: implement get tbs's path and verify hash
-function Get-TheBrokenScrpt {
+function Get-TheBrokenScript {
     param (
         LocalPath
     )
@@ -9,9 +9,24 @@ function Get-TheBrokenScrpt {
 
     # Only thebrokenscript-1.9.3-forge-1.20.1.jar
     $fileHash = ' 79D2885AB7F74698313B21A5161C74ABC55A02F3E1069080FB3A4DEF31C3A460'
+
+    # true/flase, reason
 }
 
 # TODO: unzip to temp
+function Unzip-JarFile {
+    param (
+        ValidJarPath
+    )
+}
+
+# TODO: cleanup file in assets
+# delete all and revert under specific folder
+# or maintain mapper
 
 # TODO: copy item recursively
 # use a mapper
+# logo.png => src/main/resources/assets/open-tbs/icon.png
+# assets/minecraft => ???
+# assets/thebrokenscript/sounds => src/main/resources/assets/open-tbs/sounds
+# datagen content(e.g. lang): ignore
