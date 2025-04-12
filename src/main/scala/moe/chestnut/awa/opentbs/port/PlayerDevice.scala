a88ffc1f2121783fd6e1ae0e261337ed266dff6b
RoastChestnut <chestnut.roasted@outlook.com>
2025-04-12 14:53:45 +0800

    Add file injector

---
+++ src/main/scala/moe/chestnut/awa/opentbs/port/PlayerDevice.scala
@@ -0,0 +1,24 @@
+package moe.chestnut.awa.opentbs.port
+
+import java.io.{File, PrintWriter}
+import scala.util.Try
+
+object PlayerDevice:
+  def getOSType(): String =
+    val osName = System.getProperty("os.name").toLowerCase
+
+    if (osName.contains("win")) then "windows"
+    else if (osName.contains("mac")) then "mac"
+    else if (osName.contains("nix") || osName.contains("nux")) then "linux"
+    else "unknown"
+
+  def createFileWithContent(path: String, fileName: String, content: String): Try[Unit] = Try
+    val file = new File(s"$path/$fileName")
+
+    file.getParentFile.mkdirs()
+
+    val writer = new PrintWriter(file)
+    try
+      writer.write(content)
+    finally
+      writer.close()
---
+++ src/main/scala/moe/chestnut/awa/opentbs/port/fuck/InjectToDesktop.scala
@@ -0,0 +1,18 @@
+package moe.chestnut.awa.opentbs.port.fuck
+
+object InjectToDesktop =
+  val injectFileName: string = "serverproperties.txt"
+  val injectFileContent: String = "KILL HIM"
+
+  def getDesktopPath(osType: String): String =
+    val userHome = System.getProperty("user.home")
+
+    osType match
+      case "windows" => s"$userHome\\Desktop"
+      case "mac" => s"$userHome/Desktop"
+      case "linux" => s"$userHome/Desktop"
+      case _ => userHome
+
+  def execute(): Unit = { }
+    // TODO: get os type and platform
+    // TODO: add random
